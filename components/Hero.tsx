c4a177069cba903cf14a51cc8763257e2f1470de
Supreeta-G <supreetagopal19@gmail.com>
2026-03-16 21:42:29 +0530

    curriculm cs and tcs

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -44,7 +44,7 @@ export default function Hero() {
           
         <div className="flex whitespace-nowrap animate-[ticker_20s_linear_infinite]">
             
-          <div className="flex items-center gap-12 py-2 px-6 text-white font-semibold text-lg tracking-wide">
+          <div className="flex items-center gap-6 py-1 px-4 text-white font-semibold text-sm tracking-wide">
             <span>📢 Applications for B.Sc & M.Sc Programmes 2026 will close on <span className="text-[#A3CAE9] font-bold">30 June 2026</span> – Apply Soon!</span>
             <span>📢 Applications for B.Sc & M.Sc Programmes 2026 will close on <span className="text-[#A3CAE9] font-bold">30 June 2026</span> – Apply Soon!</span>
             <span>📢 Applications for B.Sc & M.Sc Programmes 2026 will close on <span className="text-[#A3CAE9] font-bold">30 June 2026</span> – Apply Soon!</span>
